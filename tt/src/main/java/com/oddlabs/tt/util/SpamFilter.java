84b9462383cf6961afea50bca607b1aecb8981ee
RcubDev <rlinehan2@gmail.com>
2026-08-16 01:28:56 -0700

    Stop collapsing repeated punctuation in chat messages

--- tt/src/main/java/com/oddlabs/tt/util/SpamFilter.java
+++ tt/src/main/java/com/oddlabs/tt/util/SpamFilter.java
@@ -4,11 +4,7 @@ import org.jspecify.annotations.NonNull;
 
 public final class SpamFilter {
     public static @NonNull String scan(String string) {
-        string = string.replaceAll("\\s+", " ");
-        string = string.replaceAll("\\.{3,}", "…");
-        string = string.replaceAll("\\?+", "?");
-        string = string.replaceAll("\\!+", "!");
-        return string;
+        return string.replaceAll("\\s+", " ");
     }
 
     private SpamFilter() {
